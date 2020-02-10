c38d0a9e476c40db076a5e72b9b6564eb2c201c4
rkdud <rkdud@DESKTOP-UMAESQI>
2020-02-10 17:59:27 +0900

    criteria 수정

--- Mohazi/src/main/java/com/mohazi/domain/Criteria.java
+++ Mohazi/src/main/java/com/mohazi/domain/Criteria.java
@@ -11,7 +11,12 @@ public class Criteria {
    
    private int pageNum;
    private int amount;
-   private char type;
+   private char type; //클래스, 모임
+ 
+   // private String searchType; //검색  조건
+   private String keyword;
+   
+   private String id;
    
    public Criteria() {
       this(1,16);
@@ -21,4 +26,10 @@ public class Criteria {
       this.pageNum=pageNum;
       this.amount=amount;
    }
+   
+	/*
+	 * public String[] getTypeArr() {
+	 * 
+	 * return searchType == null? new String[] {}: searchType.split(""); }
+	 */
 }
